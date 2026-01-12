7a5190f2fbe5379c99e3afe2503dc6aa04f4d4f9
devilra <rockerraja906@gmail.com>
2026-01-12 20:16:36 +0530

    update ui

--- frontent/src/api.js
+++ frontent/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://localhost:5175/api`,
-  //baseURL: `https://amigowebster.in/nextshow_backend/api`,
+  //baseURL: `http://localhost:5175/api`,
+  baseURL: `https://amigowebster.in/nextshow_backend/api`,
   withCredentials: true,
 });
 
